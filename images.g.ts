c591cbf05914da6dee21749168e2d368567bf60e
Danni/petite <132593953+Bunny39@users.noreply.github.com>
2023-05-04 13:15:05 -0300

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,44 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "poewk":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . f f f 6 6 6 6 6 6 f . . . . 
+f f 8 8 8 6 5 5 6 9 1 1 f f . . 
+8 8 8 8 8 6 5 5 9 1 9 6 6 f f . 
+8 8 8 8 8 6 5 5 9 1 9 9 6 6 f . 
+8 8 8 8 8 6 1 1 9 9 9 1 1 1 f . 
+8 8 8 8 8 6 6 6 6 6 9 9 9 6 f . 
+8 8 8 8 8 f f f f 6 6 9 1 1 f . 
+8 8 8 f f . . . . f 6 9 9 6 f . 
+8 8 f . . . . . . f 6 9 1 1 f . 
+f f . . . . . . . . f 6 9 6 f . 
+. . . . . . . . . . . f 6 6 f . 
+. . . . . . . . . . . . f 6 f . 
+. . . . . . . . . . . . . f . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
